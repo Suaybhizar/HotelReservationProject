677e364f0c126c23843b0ca79dd74c87d5966f33
Şuayb Hızar <76668077+Suaybhizar@users.noreply.github.com>
2023-11-22 23:15:29 +0300

    Add project files.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,52 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+          
+            //kimden gönderilecek mail
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "coders5858@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            //kime gönderilecek
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+
+            //mesaj içeriği
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("coders5858@gmail.com", "dzwvpbcdievicave");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            //Gönderilen Mailin Veri Tabanına Kaydedilmesi.
+
+
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -0,0 +1,80 @@
+﻿using HotelProject.WebUI.Dtos.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.ViewComponents.Dashboard
+{
+    public class _DashboardSubscribeCountPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //instagram
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/suaybhizar"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "c49aaeca64msh9cb62cb11aa26efp18dfa1jsn6f80d5ae6627" },
+        { "X-RapidAPI-Host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto resultInstagramFollowersDto = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                ViewBag.v1 = resultInstagramFollowersDto.followers;
+                ViewBag.v2 = resultInstagramFollowersDto.following;
+            }
+            //Rapid api ile twitter ve linkedin hesapları için veri çekme işlemlerinde rapid api dan kaynaklanan sorun var bu nedenle instagram hesabı için kullanım gerçekleştirdim.
+            //twitter
+    //        var client2 = new HttpClient();
+    //        var request2 = new HttpRequestMessage
+    //        {
+    //            Method = HttpMethod.Get,
+    //            RequestUri = new Uri("https://twitter32.p.rapidapi.com/getProfile?username=suaybhizar"),
+    //            Headers =
+    //{
+    //    { "X-RapidAPI-Key", "630ce9cc86msh271c60cffe62d5ep1b514djsn0fe292593744" },
+    //    { "X-RapidAPI-Host", "twitter32.p.rapidapi.com" },
+    //},
+    //        };
+    //        using (var response2 = await client2.SendAsync(request2))
+    //        {
+    //            response2.EnsureSuccessStatusCode();
+    //            var body2 = await response2.Content.ReadAsStringAsync();
+    //            ResultTwittterFollowersDto resultTwittterFollowersDto = JsonConvert.DeserializeObject<ResultTwittterFollowersDto>(body2);
+    //            ViewBag.v3 = resultTwittterFollowersDto.data.user_info.followers_count;
+    //            ViewBag.v4 = resultTwittterFollowersDto.data.user_info.friends_count;
+    //        }
+
+    //        //linkedin
+    //        var client3 = new HttpClient();
+    //        var request3 = new HttpRequestMessage
+    //        {
+    //            Method = HttpMethod.Get,
+    //            RequestUri = new Uri("https://linkedin-profiles1.p.rapidapi.com/extract?url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fsuayb-hizar-430630146%2F&html=1"),
+    //            Headers =
+    //{
+    //    { "X-RapidAPI-Key", "c49aaeca64msh9cb62cb11aa26efp18dfa1jsn6f80d5ae6627" },
+    //    { "X-RapidAPI-Host", "linkedin-profiles1.p.rapidapi.com" },
+    //},
+    //        };
+    //        using (var response3 = await client3.SendAsync(request3))
+    //        {
+    //            response3.EnsureSuccessStatusCode();
+    //            var body3 = await response3.Content.ReadAsStringAsync();
+    //            ResultLinkedinFollowersDto resultLinkedinFollowersDto = JsonConvert.DeserializeObject<ResultLinkedinFollowersDto>(body3);
+    //            ViewBag.v5 = resultLinkedinFollowersDto.data.followers_count;
+    //        }
+
+
+            return View();
+        }
+    }
+}
